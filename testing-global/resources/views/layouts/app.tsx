0befedd11bd7db311611eb0e6a2747204ae0e4f9
doralb <4webtirana@gmail.com>
2026-01-05 22:08:00 +0200

    remove test project

--- testing-global/resources/views/layouts/app.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="nextlara-app">
-      {children}
-    </div>
-  );
-}
